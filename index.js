40ae7a6796c4ba10aaa725be5157ba7d01249146
Chris Chan <chrischan226@gmail.com>
2019-11-26 14:33:37 -0800

    setup Spotify Auth

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const express = require('express')
+const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
 const path = require('path');
-const app = express()
-const port = 3000;
+const helper = require('./helpers');
+const app = express();
+const port = 3001;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan('tiny'));
+var client_id = 'bb9508582785427bbe6c8aae0953cdb1'; // Your client id
+var client_secret = 'dfe82ec77feb42f982f23d31676e1e1c'; // Your secret
+var stateKey = 'spotify_auth_state';
+var redirect_uri = 'http://localhost:3001/test'; // Your redirect uri
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(__dirname + '/public'))
+   .use(cors())
+   .use(morgan('tiny'))
+   .use(cookieParser())
+   .use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/test', (req, res) => res.send('Hello World!'))
+
+app.get('/hello', function(req, res){
+    console.log(__dirname + './public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/authorize', (req, res) => {
+
+    var state = helper.generateRandomString(16);
+    var scope = 'user-read-private user-read-email';
+    res.cookie(stateKey, state);
+
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+});
+
 
 app.get('/particles.min.js', function(req, res) {
     res.sendFile(path.resolve(__dirname, './node_modules/particlesjs/dist'));
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Particles from 'particlesjs';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
@@ -7,6 +8,8 @@ class App extends React.Component {
     super(props);
 
     this.state = {};
+
+    this.login = this.login.bind(this);
   }
 
   componentDidMount() {
@@ -21,12 +24,23 @@ class App extends React.Component {
     };
   }
 
+  login() {
+    axios.get('http://localhost:3001/authorize')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
   render() {
     return (
       <div className = 'mainBoard'>
         <div className = 'directory'>
           <div className = 'title'>Flashback</div>
-          <button className = 'getStarted'>Get Started</button>
+          <button className = 'getStarted' onClick = {() => this.login()}>Get Started</button>
         </div>
         <div className = 'description'>Reminisce with your favorite songs</div>
       </div>
