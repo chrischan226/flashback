7a2a0d1c9bb1b23956fab4be2ee7bd8e302b077e
Chris Chan <chrischan226@gmail.com>
2019-11-26 22:54:27 -0800

    setup library data retrieval for users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const request = require('request');
 const cors = require('cors');
 const querystring = require('querystring');
 const cookieParser = require('cookie-parser');
@@ -9,28 +10,22 @@ const helper = require('./helpers');
 const app = express();
 const port = 3001;
 
-var client_id = 'bb9508582785427bbe6c8aae0953cdb1'; // Your client id
-var client_secret = 'dfe82ec77feb42f982f23d31676e1e1c'; // Your secret
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
 var stateKey = 'spotify_auth_state';
-var redirect_uri = 'http://localhost:3001/test'; // Your redirect uri
+var redirect_uri = 'http://localhost:3001/callback'; // Your redirect uri
 
-app.use(express.static(__dirname + '/public'))
+
+app.use(express.static(__dirname + '/build'))
    .use(cors())
    .use(morgan('tiny'))
    .use(cookieParser())
    .use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/test', (req, res) => res.send('Hello World!'))
-
-app.get('/hello', function(req, res){
-    console.log(__dirname + './public/index.html');
-    res.sendFile(__dirname + '/public/index.html');
-});
 
 app.get('/authorize', (req, res) => {
-
     var state = helper.generateRandomString(16);
-    var scope = 'user-read-private user-read-email';
+    var scope = 'user-read-private user-read-email user-library-read';
     res.cookie(stateKey, state);
 
     res.redirect('https://accounts.spotify.com/authorize?' +
@@ -41,11 +36,106 @@ app.get('/authorize', (req, res) => {
             redirect_uri: redirect_uri,
             state: state
         }));
+
+});
+
+app.get('/callback', (req, res) => {
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+		var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'state_mismatch'
+          }));
+      } else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+          url: 'https://accounts.spotify.com/api/token',
+          form: {
+            code: code,
+            redirect_uri: redirect_uri,
+            grant_type: 'authorization_code'
+          },
+          headers: {
+            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+          },
+          json: true
+				};
+
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+              
+              var access_token = body.access_token,
+									refresh_token = body.refresh_token;
+      
+              var options = {
+                url: `https://api.spotify.com/v1/me/`,
+                headers: { 'Authorization': 'Bearer ' + access_token },
+                json: true,
+              };
+
+              request.get(options, function(error, response, body) {
+                // console.log(body);
+              });
+      
+              // we can also pass the token to the browser to make requests from there
+
+              res.redirect('/#' +
+                querystring.stringify({
+                  access_token: access_token,
+                  refresh_token: refresh_token
+                }));
+            } else {
+              res.redirect('/#' +
+                querystring.stringify({
+                  error: 'invalid_token'
+                }));
+            }
+          });
+        }
+});
+
+app.get('/collectData', function(req, res) {
+	var offset = req.query.offset,
+	access_token = req.query.access,
+	refresh_token = req.query.refresh;
+
+	var options = {
+		url: `https://api.spotify.com/v1/me/tracks/?limit=50&offset=${offset}`,
+		headers: { 'Authorization': 'Bearer ' + access_token },
+		json: true,
+	};
+
+	request.get(options, function(error, response, body) {
+		res.send(body);
+	});
+
 });
 
 
-app.get('/particles.min.js', function(req, res) {
-    res.sendFile(path.resolve(__dirname, './node_modules/particlesjs/dist'));
+app.get('/refresh_token', function(req, res) {
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token;
+        res.send({
+          'access_token': access_token
+        });
+      }
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
--- src/App.js
+++ src/App.js
@@ -7,13 +7,42 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      authorized: false,
+      code: '',
+      state: '',
+      library: {},
+      full: false,
+    };
 
     this.login = this.login.bind(this);
+    this.collectData = this.collectData.bind(this);
   }
 
   componentDidMount() {
-    window.onload = function() {
+    const { authorized } = this.state;
+    let url = window.location.href;
+    let state,
+        code;
+
+    if(url.includes('code') && url.includes('state')) {
+      state = url.slice(url.indexOf('&state=') + 7);
+      code = url.slice(url.indexOf('=') + 1, url.indexOf('&state='));
+      
+      this.setState({
+        authorized: true,
+        code,
+        state,
+      });
+    }
+
+    if(url.includes('access_token') && url.includes('refresh_token')) {
+      let refresh = url.slice(url.indexOf('&refresh_token=') + 15);
+      let access = url.slice(url.indexOf('=') + 1, url.indexOf('&refresh_token='));
+      this.collectData(access, refresh, 0);
+    }
+
+    if(!authorized) {
       Particles.init({
         selector: '.background',
         speed: 0.7,
@@ -21,29 +50,72 @@ class App extends React.Component {
         minDistance: 200,
         connectParticles: true,
       });
-    };
+    }
+  }
+
+  collectData(access, refresh, offset) {
+    const { full, library } = this.state;
+    let storage = Object.assign({}, library);
+
+    if(!full) {
+      axios.get(`/collectData/`, {
+        params: {
+          access,
+          refresh,
+          offset: offset,
+        }
+      })
+      .then(res => {
+          for(let i = 0; i < res.data.items.length; i++) {
+            let year = res.data.items[0].added_at.slice(0,4);
+            let month = res.data.items[0].added_at.slice(5, 7);
+
+            if(storage[year] === undefined) storage[year] = {};
+            if(storage[year][month] === undefined) storage[year][month] = [];
+
+            storage[year][month].push([res.data.items[i].track.artists[0].name, res.data.items[i].track.name, res.data.items[i].track.uri]);
+          }
+          if(res.data.items.length === 50) {
+            this.setState({
+              library: storage,
+              offset: offset + 50,
+            }, () => this.collectData(access, refresh, offset + 50))
+          } else {
+            this.setState({
+              library: storage,
+              offset: offset + res.data.items.length,
+              full: true,
+            })
+          }
+      })
+      .catch(err => console.log(err));
+    }
   }
 
   login() {
-    axios.get('http://localhost:3001/authorize')
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+     window.location.href = 'http://localhost:3001/authorize';
   }
 
 
   render() {
+    const { authorized } = this.state;
     return (
-      <div className = 'mainBoard'>
-        <div className = 'directory'>
-          <div className = 'title'>Flashback</div>
-          <button className = 'getStarted' onClick = {() => this.login()}>Get Started</button>
+    <div>
+      {!authorized ? 
+        <div>
+        <canvas class="background"></canvas>
+          <div className = 'mainBoard'>
+            <div className = 'directory'>
+              <div className = 'title'>Flashback</div>
+              <button className = 'getStarted' onClick = {this.login}>Get Started</button>
+            </div>
+            <div className = 'description'>Reminisce with your favorite songs</div>
+          </div>
         </div>
-        <div className = 'description'>Reminisce with your favorite songs</div>
-      </div>
+        :
+        <div>Authorized</div>
+      }
+    </div>
     )
   }
 }
